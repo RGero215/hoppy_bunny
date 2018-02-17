19ab9c6340759cfbd876dbf2bae58ffb5b3c99a5
RGero215 <rgero215@gmail.com>
2018-02-17 16:26:26 -0400

    init: set up the game scene
    So far the bunny drops to the ground quickly and there is no player control added.

---
+++ Hoppy Bunny/GameScene.swift
@@ -0,0 +1,25 @@
+//
+//  GameScene.swift
+//  Hoppy Bunny
+//
+//  Created by Ramon Geronimo on 2/17/18.
+//  Copyright © 2018 Ramon Geronimo. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        /* Setup your scene here */
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        /* Called when a touch begins */
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
